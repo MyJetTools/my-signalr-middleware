b88ff9cc2c8587ce868b23525c4ad3403c9b6afd
amigin <amigin@gmail.com>
2023-09-13 14:55:51 +0300

    Debugging Ping Payload

--- src/web_socket_callbacks.rs
+++ src/web_socket_callbacks.rs
@@ -111,7 +111,7 @@ impl<TCtx: Send + Sync + Default + 'static> my_http_server_web_sockets::MyWebSoc
                     }
 
                     if packet_type == "6" {
-                        signalr_connection.update_incoming_activity();
+                        signalr_connection.send_ping_payload().await;
                     }
                 } else {
                     read_first_payload(signalr_connection, value).await
